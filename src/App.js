a96f4fe06c0190a6968bfb058602517d9d0646be
joverandout <joverandout@gmail.com>
2022-01-17 15:28:24 +0000

    button finally working arghhhh

--- src/App.js
+++ src/App.js
@@ -8,16 +8,17 @@ function App() {
   const [SetFunctionOutput, setNewFunctionOutput] = useState(0);
 
 
-  useEffect(() => {
-      // Simple POST request with a JSON body using fetch
-      const requestOptions = {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({ title: 'return("hello");' })
-      };
-      fetch('/write', requestOptions)
-          .then(response => response.json());
-  }, []);
+  // useEffect(() => {
+  //     // Simple POST request with a JSON body using fetch
+  //     const requestOptions = {
+  //         method: 'POST',
+  //         headers: { 'Content-Type': 'application/json' },
+  //         body: JSON.stringify({ title: 'return("yo"+"yo");' })
+  //     };
+  //     fetch('/write', requestOptions)
+  //         .then(response => response.json());
+  // }, []);
+
 
 
   useEffect(() => {
@@ -27,6 +28,18 @@ function App() {
   }, []);
 
 
+  function buttonpress(){
+    // Simple POST request with a JSON body using fetch
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ title: 'return("hello2");' })
+  }
+
+  fetch('/write', requestOptions)
+      .then(response => response.json());
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -34,6 +47,10 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <button onClick={buttonpress}>
+          Click me!
+        </button>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -44,6 +61,37 @@ function App() {
         </a>
         <p>The current time is {FunctionOutput}.</p>
       </header>
+      <div class="hero-head">
+            <nav class="navbar">
+                <div class="container">
+                    <div id="navbarMenuHeroA" class="navbar-menu">
+                        <div class="navbar-end">
+                            <a href="{{ url_for('main.index') }}" class="navbar-item">
+                                Home
+                            </a>
+                            <a href="{{ url_for('main.profile') }}" class="navbar-item">
+                                Profile
+                            </a>
+                            <a href="{{ url_for('auth.login') }}" class="navbar-item">
+                                Login
+                            </a>
+                            <a href="{{ url_for('auth.signup') }}" class="navbar-item">
+                                Sign Up
+                            </a>
+                            <a href="{{ url_for('auth.logout') }}" class="navbar-item">
+                                Logout
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </div>
+
+        <div class="hero-body">
+            <div class="container has-text-centered">
+               
+            </div>
+        </div>
     </div>
   );
 }
