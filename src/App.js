0355548faf95928362121ef590d4cb61fcbcc2a0
joverandout <joverandout@gmail.com>
2022-02-07 11:53:00 +0000

    login page sorted

--- src/App.js
+++ src/App.js
@@ -5,13 +5,23 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Dashboard from './components/Dashboard/Dashboard';
 import Login from './components/Login/Login';
 import Preferences from './components/Preferences/Preferences';
+import useToken from './useToken';
+
+function setToken(userToken) {
+  sessionStorage.setItem('token', JSON.stringify(userToken));
+}
+
+function getToken() {
+  const tokenString = sessionStorage.getItem('token');
+  const userToken = JSON.parse(tokenString);
+  return userToken?.token
+}
 
 function App() {
-
+  const { token, setToken } = useToken();
   const [FunctionOutput, setFunctionOutput] = useState(0);
   const [PrintOutputs, setPrintOutput] = useState(0);
   const [name, setName] = useState('');
-  const [token, setToken] = useState();
   const editor = document.querySelector(".editor");
 
 
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,23 +1,53 @@
-import React from 'react';
+import React, {useState} from 'react';
+import PropTypes from 'prop-types';
+
+
 import './Login.css';
 
-export default function Login() {
-  return(
-    <div className="login-wrapper">
-      <h1>Please Log In</h1>
-      <form>
-        <label>
-          <p>Username</p>
-          <input type="text" />
-        </label>
-        <label>
-          <p>Password</p>
-          <input type="password" />
-        </label>
-        <div>
-          <button type="submit">Submit</button>
-        </div>
-      </form>
-    </div>
-  )
+async function loginUser(credentials) {
+    return fetch('http://localhost:8080/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(credentials)
+    })
+      .then(data => data.json())
 }
+
+export default function Login({ setToken }) {
+    const [username, setUserName] = useState();
+    const [password, setPassword] = useState();
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        const token = await loginUser({
+          username,
+          password
+        });
+        setToken(token);
+    }
+    
+    return(
+        <div className="login-wrapper">
+            <h1>Please Log In</h1>
+            <form onSubmit={handleSubmit}>
+            <label>
+                <p>Username</p>
+                <input type="text" onChange={e => setUserName(e.target.value)} />
+            </label>
+            <label>
+                <p>Password</p>
+                <input type="password" onChange={e => setPassword(e.target.value)}/>
+            </label>
+            <div>
+                <button type="submit">Submit</button>
+            </div>
+            </form>
+        </div>
+    )
+}
+
+Login.propTypes = {
+    setToken: PropTypes.func.isRequired
+  }
