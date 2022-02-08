1d6ba0052972a8d213bbfb077f2eb30b9aa4fb37
joverandout <joverandout@gmail.com>
2022-02-08 11:38:31 +0000

    login done

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -29,20 +29,25 @@ export default function Login({ setToken }) {
     }
     
     return(
+      
         <div className="login-wrapper">
-            <h1>Please Log In</h1>
+            <h1>Hale - A Web Based Interpreter</h1>
+            <h2>Please login</h2>
             <form onSubmit={handleSubmit}>
-            <label>
-                <p>Username</p>
-                <input type="text" onChange={e => setUserName(e.target.value)} />
-            </label>
-            <label>
-                <p>Password</p>
-                <input type="password" onChange={e => setPassword(e.target.value)}/>
-            </label>
-            <div>
-                <button type="submit">Submit</button>
-            </div>
+              <div class="form-group">
+                <label for="exampleInputEmail1">Email address</label>
+                <input type="email" class="form-control input-lg" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" onChange={e => setUserName(e.target.value)}></input>
+                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+              </div>
+              <div class="form-group">
+                <label for="exampleInputPassword1">Password</label>
+                <input type="password" class="form-control input-lg" id="exampleInputPassword1" placeholder="Password" onChange={e => setPassword(e.target.value)}></input>
+              </div>
+              <div class="form-check">
+                <input type="checkbox" class="form-check-input input-lg" id="exampleCheck1"></input>
+                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+              </div>
+              <button type="submit" class="btn btn-primary">Submit</button>
             </form>
         </div>
     )
