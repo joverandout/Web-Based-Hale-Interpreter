2dc8b3750bbe577eb1c1f1b7ae0b3ab5250d0638
joverandout <joverandout@gmail.com>
2022-03-08 11:13:13 +0000

    proxy change

--- api/api.py
+++ api/api.py
@@ -181,41 +181,44 @@ def save_file():
 @app.route('/hostlogin', methods=["POST"])
 def hostlogin():
     info = request.get_json()
-    if info == None:
-        return ("No login information was provided",400)
+    # if info == None:
+    #     return ("No login information was provided",400)
 
-    #Dont actually know what to do if parsing fails. info will be an error
-    try:
-        username = info["email"]
-        password = info["password"]
+    # #Dont actually know what to do if parsing fails. info will be an error
+    # try:
+    #     username = info["email"]
+    #     password = info["password"]
 
-        hashed_password = hashlib.sha256(password.encode('utf-8')).hexdigest()
-        print(hashed_password)
-        # print(hashed_password)
-        succesful_login = False
+    #     hashed_password = hashlib.sha256(password.encode('utf-8')).hexdigest()
+    #     print(hashed_password)
+    #     # print(hashed_password)
+    #     succesful_login = False
 
-        #logic to determine if the user is in the database,
-        try:
-            with sqlite3.connect("APIData.db") as con:
-                cur = con.cursor()
-                query = "SELECT Username, Password FROM USERS WHERE username = '" + username + "'"
-                cur.execute(query)
-                data = cur.fetchall()
-                if data[0][1] == hashed_password:
-                    succesful_login = True
-                    #DO THIS RETURN HOST ID 
-                    returnDict = dict()
-                    returnDict["token"] = 'token1234'
-                    returnDict["username"] = username
-                    return jsonify(returnDict)
-        except:
-            return ("Email address does not exist", 401)
+    #     #logic to determine if the user is in the database,
+    #     try:
+    #         with sqlite3.connect("APIData.db") as con:
+    #             cur = con.cursor()
+    #             query = "SELECT Username, Password FROM USERS WHERE username = '" + username + "'"
+    #             cur.execute(query)
+    #             data = cur.fetchall()
+    #             if data[0][1] == hashed_password:
+    #                 succesful_login = True
+    #                 #DO THIS RETURN HOST ID 
+    #                 returnDict = dict()
+    #                 returnDict["token"] = 'token1234'
+    #                 returnDict["username"] = username
+    #                 return jsonify(returnDict)
+    #     except:
+    #         return ("Email address does not exist", 401)
         
-        if(not succesful_login):
-            return ("Incorrect password",400)
-    except:
-        #Likely error is that the request did not have the fields we wanted from it
-        return ("Cannot retrieve data, please check credentials", 400)
+    #     if(not succesful_login):
+    #         return ("Incorrect password",400)
+    # except:
+    #     #Likely error is that the request did not have the fields we wanted from it
+    #     return ("Cannot retrieve data, please check credentials", 400)
+    returnDict = dict()
+    returnDict["token"] = 'token1234'
+    returnDict["username"] = 'joverandout@gmail.com' 
 
 
 @app.route('/hostSignUp', methods=["POST"])
