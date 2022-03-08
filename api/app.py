22579c2e443b44e474646f59c5811ab8ba92ecf1
joverandout <joverandout@gmail.com>
2022-03-08 11:19:34 +0000

    proxy change

--- api/app.py
+++ api/app.py
@@ -285,5 +285,5 @@ def usercreate():
     if info == None:
         return "No login information was provided"
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=False, port=int(os.environ.get("PORT", 5000)))
+if __name__ == '__main__':
+    app.run()
