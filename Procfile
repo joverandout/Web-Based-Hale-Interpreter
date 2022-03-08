7717c67ff0924c1ab930518a4a6d7ad6ddaa9ad6
joverandout <joverandout@gmail.com>
2022-03-08 14:44:14 +0000

    added procfile

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: gunicorn app:app
+web: gunicorn server:flask_app
--- api/api.py
+++ api/api.py
@@ -285,5 +285,9 @@ def usercreate():
     if info == None:
         return "No login information was provided"
 
-if __name__ == '__main__':
-    app.run()
+@app.errorhandler(404)
+def not_found(e):
+    return app.send_static_file('index.html')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
